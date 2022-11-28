9853b81255c6a4a99c6c8339485b78fc68b66313
valentincx <33153750+valentincx@users.noreply.github.com>
2022-11-28 17:19:58 +0100

    [WIP] Basic functionnalities
    Add, remove without db connection/storage.
    Routing WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {v4 as uuidv4} from 'uuid'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {useState} from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
+
+import FeedbackData from './data/FeedbackData'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [feedback, setFeedBack] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedBack([newFeedback,...feedback])
+    }
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')) {
+            setFeedBack(feedback.filter((item) => item.id !== id))
+        }
+    }
+
+    return (
+        <Router>
+            <Header />
+            <div className="container">
+                <Routes>
+                <Route exact path='/' element={
+                <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats  feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} /></>
+            }>
+                
+                </Route>
+                <Route path='/about' element={<AboutPage/>}/>
+                </Routes>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,21 @@
+import { FaTimes } from 'react-icons/fa'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
+
+function FeedBackItem({ item, handleDelete }) {
+  return (
+    <Card>
+      <div className='num-display'>{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
+      <div className='text-display'>{item.text}</div>
+    </Card>
+  )
+}
+
+FeedBackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+}
+
+export default FeedBackItem
